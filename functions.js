3bbb8994d4c6c492f727d4c33003c36407abc21f
Stefano Casasola <ste.casasola@gmail.com>
2025-02-13 13:08:44 +0100

    Finishes today's lecture

--- functions.js
+++ functions.js
@@ -124,3 +124,49 @@ tellYourAge(19) // "Io ho 19 anni."
 tellYourAge(anotherSum()) // "Io ho 30 anni."
 // sono riuscito ad utilizzare il valore calcolato (e ritornato) da anotherSum come
 // parametro di un'altra funzione! :O
+
+const verificaEta = function (age) {
+  if (age < 18) {
+    // sei minorenne
+    return 'minorenne'
+  } else {
+    // sei maggiorenne
+    return 'maggiorenne'
+  }
+}
+
+verificaEta(20) // 'maggiorenne'
+verificaEta(17) // 'minorenne'
+
+const esamePatente = function (idoneo) {
+  if (idoneo === 'maggiorenne') {
+    console.log('Benvenuto al tuo esame della patente')
+  } else {
+    console.log('NON AUTORIZZATO')
+  }
+}
+
+esamePatente('maggiorenne') // 'Benvenuto al tuo esame della patente'
+esamePatente('minorenne') // 'NON AUTORIZZATO'
+esamePatente(verificaEta(15)) // 'NON AUTORIZZATO'
+esamePatente(verificaEta(25)) // 'Benvenuto al tuo esame della patente'
+
+// --------------
+// METODI DELLE STRINGHE
+const str1 = 'CIAO EPICODE COME VA'
+const p1 = str1.indexOf('CIAO')
+// p1 è 0
+console.log('P1 è', p1)
+const p2 = str1.slice(5, 12)
+console.log('P2 è', p2)
+
+const str2 = 'CIAONE'
+const str3 = 'A TUTTI GLI STUDENTI'
+
+const str4 = str2 + ' ' + str3 // 'CIAONE A TUTTI GLI STUDENTI'
+const str5 = str2.concat(' ', str3) // 'CIAONE A TUTTI GLI STUDENTI'
+
+str2.concat(' ', str3).toLowerCase() // 'ciaone a tutti gli studenti'
+
+const ciao = str2.concat(' ', str3).toLowerCase().slice(0, 4) // 'ciao'
+console.log(ciao)
